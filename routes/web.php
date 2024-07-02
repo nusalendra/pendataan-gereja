d477bf1eb6414fa581f9a67c956915457feb311e
Nusalendra <nusalendraa@gmail.com>
2024-07-02 11:37:56 +0700

    fix:menghilangkan menu yang tidak terpakai

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,9 @@ use App\Http\Controllers\Jemaat\PendaftaranBaptisController;
 use App\Http\Controllers\Jemaat\PendaftaranMenikahController;
 use App\Http\Controllers\Jemaat\PendaftaranSidiController;
 use App\Http\Controllers\Jemaat\ProfilController;
-use App\Http\Livewire\Auth\ForgotPassword;
 use App\Http\Livewire\Auth\Login;
-use App\Http\Livewire\Auth\Register;
-use App\Http\Livewire\Auth\ResetPassword;
-use App\Http\Livewire\Billing;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Dashboard;
-use App\Http\Livewire\ExampleLaravel\UserManagement;
-use App\Http\Livewire\ExampleLaravel\UserProfile;
-use App\Http\Livewire\Notifications;
-use App\Http\Livewire\Profile;
-use App\Http\Livewire\RTL;
-use App\Http\Livewire\StaticSignIn;
-use App\Http\Livewire\StaticSignUp;
-use App\Http\Livewire\Tables;
-use App\Http\Livewire\VirtualReality;
-use GuzzleHttp\Middleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,15 +26,7 @@ use GuzzleHttp\Middleware;
 */
 
 Route::group(['middleware' => ['guest']], function () {
-    Route::get('/', function () {
-        return redirect('sign-in');
-    });
-    
-    Route::get('/', Login::class)->middleware('guest')->name('login');
-    
-    // Route::get('sign-up', Register::class)->middleware('guest')->name('register');
-    // Route::get('forgot-password', ForgotPassword::class)->middleware('guest')->name('password.forgot');
-    // Route::get('reset-password/{id}', ResetPassword::class)->middleware('signed')->name('reset-password');
+    Route::get('/', Login::class)->name('login');
 });
 
 
@@ -81,18 +59,6 @@ Route::group(['middleware' => ['auth']], function () {
 
         Route::get('/laporan', [LaporanController::class, 'index'])->name('laporan');
         Route::get('/laporan/cetak-pdf', [LaporanController::class, 'cetakPDF'])->name('laporan-cetak-pdf');
-
-        Route::get('billing', Billing::class)->name('billing');
-        Route::get('profile', Profile::class)->name('profile');
-        Route::get('tables', Tables::class)->name('tables');
-        Route::get('notifications', Notifications::class)->name("notifications");
-        Route::get('virtual-reality', VirtualReality::class)->name('virtual-reality');
-        Route::get('static-sign-in', StaticSignIn::class)->name('static-sign-in');
-        Route::get('static-sign-up', StaticSignUp::class)->name('static-sign-up');
-        Route::get('rtl', RTL::class)->name('rtl');
-
-        Route::get('user-profile', UserProfile::class)->middleware('auth')->name('user-profile');
-        Route::get('user-management', UserManagement::class)->middleware('auth')->name('user-management');
     });
 
     Route::group(['middleware' => 'role:Jemaat'], function () {
